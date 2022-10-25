ab65ba65fd2d89d793886b1d7648c288ff866970
Karan <rkrana811@gmail.com>
2022-10-25 18:55:01 -0500

    is triangle page finished test

--- triangles.js
+++ triangles.js
@@ -2,6 +2,9 @@ const angles = document.querySelectorAll(".angle-input");
 const button = document.querySelector(".checktriangle");
 const message = document.querySelector(".message");
 
+
+
+
 button.addEventListener("click", function istriangle(a, b, c) {
     a = Number(angles[0].value);
     b = Number(angles[1].value);
