fdc684ff40c074f54a19df02fe5119e4d1842ad0
Karan <rkrana811@gmail.com>
2022-10-26 19:38:09 -0500

    styled all pages

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ const quizform = document.querySelector(".quizform");
 const submit = document.querySelector(".submit");
 const message = document.querySelector(".message");
 
-const correctAnswers = ["35°", "Right", "Equilateral", "25", "178°", "Equilateral", "3,4,5", "Three", "300 units square","0.5"];
+const correctAnswers = ["35°", "Right", "Equilateral", "25", "178°", "Equilateral", "3,4,5", "Three", "300 units","0.5"];
 
 function calculateScore() {
     const formSubmission = new FormData(quizform);
